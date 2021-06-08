d7a1e28989170550c65c4c6fbf91dc8a7bc3a3c6
Anuj040 <anujarora920804@gmail.com>
2021-06-08 17:52:08 +0900

    chore: define data loader class

---
+++ src/utils/data_handler.py
@@ -0,0 +1,10 @@
+"""module for extracting, preporcessing, etc. of data"""
+
+
+class DataLoader:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    loader = DataLoader()
