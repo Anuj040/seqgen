35819ae7f43cf24121bd900f78ea95d23c213dde
Anuj040 <anujarora920804@gmail.com>
2021-06-09 11:15:50 +0900

    chore: define gen_num_seq method

---
+++ src/number_generator.py
@@ -0,0 +1,9 @@
+"""module for different (sequence, phone-number) generator functions"""
+
+
+def generate_numbers_sequence():
+    pass
+
+
+if __name__ == "__main__":
+    generate_numbers_sequence()
